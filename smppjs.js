455ba6db2308c835fa46fbba9eb67814b96a498d
ldev <ldev@ldev.eu.org>
2023-11-15 14:26:07 +0100

    dmenu things'

--- smppjs.js
+++ smppjs.js
@@ -10,7 +10,10 @@ function dmenu(params) {
   let dmenu = document.getElementById("dmenu");
   dmenu.classList.remove("dmenu-hidden");
   dmenu.getElementsByTagName("input")[0].focus();
-  console.log("dmenu");
+}
+function dmenu_close() {
+  let dmenu = document.getElementById("dmenu");
+  dmenu.classList.add("dmenu-hidden");
 }
 
 function init_dmenu(){
@@ -18,11 +21,15 @@ function init_dmenu(){
   dmenu.id="dmenu";
   dmenu.classList.add("dmenu");
   dmenu.classList.add("dmenu-hidden");
-  dmenu.innerHTML="<input type='text'>";
+  dmenu.innerHTML="<div class='top'><label class='dmenu-label'>dmenu:</label><input class='dmenu-input' type='text'></div>";
   document.body.insertBefore(dmenu, document.body.childNodes[-1]);
-  dmenu.getElementsByTagName("input")[0].attachEventListener("keydown", function(e)
+  dmenu.getElementsByTagName("input")[0].addEventListener("keydown", function(e)
     {
-      
+      if (e.key == "Enter"){
+        dmenu_close();
+      }else if (e.key == "Escape"){
+        dmenu_close();
+      }
     });
 
 }
