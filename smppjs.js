e0e2150f8827f7b9ffbe30c7a552b03ed69c2926
ldev <ldev@ldev.eu.org>
2023-11-15 14:16:13 +0100

    started on dmenu

--- smppjs.js
+++ smppjs.js
@@ -5,4 +5,33 @@ function unbloat() {
   document.body.innerHTML = '';
 }
 
-//unbloat
+
+function dmenu(params) {
+  let dmenu = document.getElementById("dmenu");
+  dmenu.classList.remove("dmenu-hidden");
+  dmenu.getElementsByTagName("input")[0].focus();
+  console.log("dmenu");
+}
+
+function init_dmenu(){
+  let dmenu = document.createElement("div");
+  dmenu.id="dmenu";
+  dmenu.classList.add("dmenu");
+  dmenu.classList.add("dmenu-hidden");
+  dmenu.innerHTML="<input type='text'>";
+  document.body.insertBefore(dmenu, document.body.childNodes[-1]);
+  dmenu.getElementsByTagName("input")[0].attachEventListener("keydown", function(e)
+    {
+      
+    });
+
+}
+
+init_dmenu();
+
+document.addEventListener("keyup", function(e){
+  console.log(e);
+  if (e.key == ':'){
+    dmenu(["unbloat"]);
+  }
+});
