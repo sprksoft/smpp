16f7cbb1e7b6aa5885fa7e7694efb6dbd6360465
ldev <ldev@ldev.eu.org>
2023-11-17 21:32:25 +0100

    added bad code warnigns

--- smppjs.js
+++ smppjs.js
@@ -4,20 +4,20 @@
 
 function unbloat() {
   document.body.innerHTML = '';
-}try{
+}
+
+//WARNING: garbage
+try{
+  document.getElementsByClassName("js-btn-logout")[0].innerHTML = "Logout -->";
   document.getElementById("notifsToggleLabel").innerHTML = "Toon pop-ups"
 }catch(e){}
+
 document.addEventListener("keyup", function(e){
   console.log(e);
   if (e.key == ':'){
     dmenu(["unbloat"]);
   }
 });
-
-try{
-  document.getElementsByClassName("js-btn-logout")[0].innerHTML = "Logout -->";
-}catch(e){};
-
 
 
 chrome.storage.local.get('current_profile', function(store) {
