d2a93d9e6f48337f17f2af40c92038e0d16527b5
frickingbird8002 <sakul8002@icloud.com>
2023-11-17 16:45:39 +0100

    popup added and minor fixes

--- smppjs.js
+++ smppjs.js
@@ -15,3 +15,22 @@ document.addEventListener("keyup", function(e){
 
 
 document.getElementsByClassName("js-btn-logout")[0].innerHTML = "Logout -->"
+//popupjs
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("profileSelector").addEventListener("change", function() {
+    var selectedProfile = document.getElementById("profileSelector").value;
+    console.log("Chosen profile: " + selectedProfile);
+    switch (selectedProfile) {
+      case "profile1":
+        // Code to set variables for profile 1
+        break;
+      case "profile2":
+        // Code to set variables for profile 2
+        break;
+      case "profile3":
+        // Code to set variables for profile 3
+        break;
+    }
+    location.reload();
+  });
+});
