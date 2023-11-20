ebb535530d157c4f3617794a87e1656c7c4b87c1
frickingbird8002 <sakul8002@icloud.com>
2023-11-20 19:43:05 +0100

    weather works, now it needs t look good

--- weather.js
+++ weather.js
@@ -34,36 +34,36 @@ async function getWeatherByLocation() {
   }
 }
 
-// Function to update the 'rightcontainer' div with weather information
 async function updateWeatherDiv() {
     console.log('Updating weather information based on user location...');
     const weatherData = await getWeatherByLocation();
     if (weatherData) {
-      console.log('Weather data:', weatherData);
-      const rightContainer = document.getElementById('rightcontainer');
-      const { name, main, weather, rain, snow } = weatherData; // Include 'rain' and 'snow' properties
-  
-      const temperature = Math.round(main.temp);
-      const description = weather[0].description;
-      let precipitationChance = 'N/A'; // Default value for precipitation chance
-  
-      if (rain && rain['1h']) {
-        // If it's raining, consider high chance of precipitation
-        precipitationChance = 'High';
-      } else if (snow && snow['1h']) {
-        // If it's snowing, consider moderate chance of precipitation
-        precipitationChance = 'Moderate';
-      }
-  
-      // Update the content of the div
-      rightContainer.innerHTML = `
-        <h2>Weather in ${name}</h2>
-        <p>Temperature: ${temperature}°C</p>
-        <p>Description: ${description}</p>
-        <p>Precipitation Chance: ${precipitationChance}</p> <!-- Display precipitation chance -->
-      `;
+        console.log('Weather data:', weatherData);
+        const rightContainer = document.getElementById('rightcontainer');
+        const { name, main, weather, rain, wind } = weatherData; // Include rain data
+        const temperature = Math.round(main.temp);
+        const description = weather[0].description;
+        const rainVolume = rain ? rain['1h'] : 0; // Check if rain data
+        const humidity = main.humidity;
+        const minTemp = Math.round(main.temp_min);
+        const maxTemp = Math.round(main.temp_max);
+        const windSpeed = wind.speed;
+        const mainWeather = weather[0].main;
+
+        // Update the content of the div
+        rightContainer.innerHTML = `
+            <h2>Weather in ${name}</h2>
+            <p>Temperature: ${temperature}°C</p>
+            <p>Main Weather: ${mainWeather}</p>
+            <p>Description: ${description}</p>
+            <p>Humidity: ${humidity}%</p>
+            <p>Min Temperature: ${minTemp}°C</p>
+            <p>Max Temperature: ${maxTemp}°C</p>
+            <p>Wind Speed: ${windSpeed} m/s</p>
+            <p>Rain (last hour): ${rainVolume} mm</p> <!-- Display rain volume -->
+        `;
     }
-  }
-  
+}
+
 // Call the function to update the 'rightcontainer' div based on user's location
 updateWeatherDiv();
