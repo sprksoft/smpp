040cca4df639d6a0f6ee52ff74f15812204d825b
ldev <your.mom@gmail.com>
2023-11-24 19:37:43 +0100

    popup voor lukas te fixen

--- POPUP/popup.js
+++ POPUP/popup.js
@@ -1,13 +1,17 @@
 //popupjs
 document.addEventListener("DOMContentLoaded", function () {
-  let profileSelect=document.getElementById("profileSelector");
-  let input=document.getElementById("input");
+  let profileSelect = document.getElementById("profileSelector");
+  let input = document.getElementById("input");
+  let haltes = document.getElementById("halt"); // Fix: Change "haltes" to "halt"
+  let idSelect = document.getElementById("idSelector");
+
   chrome.storage.local.get("current_profile", function (store) {
-    profileSelect.value=store.current_profile;
-  })
+    profileSelect.value = store.current_profile;
+  });
+
   chrome.storage.local.get("local_background", function (store) {
-    input.value=store.local_background;
-  })
+    input.value = store.local_background;
+  });
 
   profileSelect.addEventListener("change", function () {
     var selectedProfile = document.getElementById("profileSelector").value;
@@ -17,14 +21,40 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     console.log("set profile to " + selectedProfile);
-
   });
+
   input.addEventListener("change", function () {
     var background = document.getElementById("input").value;
     chrome.storage.local.set({
-      local_background: background
+      local_background: background,
     });
-    
-    console.log("set backround to " + background);
+
+    console.log("set background to " + background);
+  });
+
+  chrome.storage.local.get("current_id", function (store) {
+    idSelect.value = store.current_id;
+  });
+
+  chrome.storage.local.get("local_id", function (store) {
+    haltes.value = store.local_id; // Fix: Change "haltes" to "halt"
+  });
+
+  haltes.addEventListener("change", function () {
+    var selectedId = document.getElementById("idSelector").value;
+
+    chrome.storage.local.set({
+      current_id: selectedId,
+    });
+
+    console.log("set bus id to " + selectedId);
+  });
+
+  haltes.addEventListener("change", function () {
+    var halte = document.getElementById("halt").value; // Fix: Change "haltes" to "halt"
+    chrome.storage.local.set({
+      local_id: halte,
+    });
   });
 });
+
