731ea3b3162b80ccef8344914a2a96e364f2a85e
ldev <your.mom@gmail.com>
2023-11-26 11:59:12 +0100

    Lukas gedaan

--- POPUP/popup.js
+++ POPUP/popup.js
@@ -4,7 +4,21 @@ document.addEventListener("DOMContentLoaded", function () {
   let input = document.getElementById("input");
   let halte = document.getElementById("halt");
   let button = document.getElementById("button");
+  function refresh(){
 
+        if (typeof chrome !== 'undefined' && chrome.tabs) {
+          // Chrome
+          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.reload(tabs[0].id);
+          });
+        } else if (typeof browser !== 'undefined' && browser.tabs) {
+          // Firefox
+          browser.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
+            browser.tabs.reload(tabs[0].id);
+          });
+        }
+      };
+    
   chrome.storage.local.get("current_profile", function (store) {
     profileSelect.value = store.current_profile;
   });
@@ -19,6 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
     chrome.storage.local.set({
       current_profile: selectedProfile,
     });
+    refresh()
 
     console.log("set profile to " + selectedProfile);
   });
@@ -28,6 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
     chrome.storage.local.set({
       local_background: background,
     });
+    refresh()
 
     console.log("set background to " + background);
   });
@@ -36,6 +52,8 @@ document.addEventListener("DOMContentLoaded", function () {
     chrome.storage.local.set({
       current_id: halte,
     });
+    refresh()
+
     console.log("set halte to " + halte);
   });
   button.addEventListener("change", function () {
@@ -43,6 +61,8 @@ document.addEventListener("DOMContentLoaded", function () {
     chrome.storage.local.set({
       buttonstate: button,
     });
+    refresh()
+
     console.log("set button to " + button);
   });
   chrome.storage.local.get("current_id", function (store) {
