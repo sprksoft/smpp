42e44944bdcd50bc840ec61d5ec8b779d52b6504
frickingbird8002 <sakul8002@icloud.com>
2023-11-28 01:12:22 +0100

    login is now good(i also hate the white)

---
+++ loginpage.js
@@ -0,0 +1,2 @@
+document.getElementsByClassName('login-app__left')[0].innerHTML = ' '
+document.getElementsByClassName('login-app__platform-indicator')[0].innerHTML = '<h1 class="logintitle">Smartschool ++</h1>'
