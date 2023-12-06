e2bf8457c58f1459e93c79eb6f7c33e8e1dad168
ldev <ldev@ldev.eu.org>
2023-12-06 15:23:18 +0100

    Forgot to add that file

--- smppjs.js
+++ smppjs.js
@@ -40,67 +40,6 @@ get_data_bg("vakken", ".course-list > li > a", function (el, data) {
   vakken = data;
 });
 
-function get_config() {
-  return JSON.parse(window.localStorage.getItem("settingsdata"));
-}
-function set_config(config) {
-  window.localStorage.setItem("settingsdata", JSON.stringify(config));
-  apply();
-}
-
-function config_menu() {
-  let conf = get_config();
-  let cmd_list = Object.keys(conf);
-  dmenu(cmd_list, function(cmd, shift) {
-    conf[cmd] = dmenu([], function (val, shift) {
-      conf[cmd] = val;
-      set_config(conf);
-    }, "value:")
-  }, "config: ");
-}
-
-document.addEventListener("keyup", function (e) {
-  if (e.key == ':') {
-    let cmd_list = Object.keys(vakken).concat(Object.keys(goto_items).concat(["classroom", "onshape", "config", "set theme v2", "lock dmenu", "unbloat"]));
-    dmenu(cmd_list, function (cmd, shift) {
-      switch (cmd) {
-        case "lock dmenu":
-          lock_dmenu = !lock_dmenu;
-          return;
-        case "unbloat":
-          unbloat();
-          return;
-        case "set background":
-          dmenu([], function (url) {
-            set_background(url);
-            store_background(url);
-          }, "bg url:");
-          return;
-        case "config":
-          config_menu();
-          return;
-        case "classroom":
-          open_url("https://classroom.google.com", shift);
-          return;
-        case "onshape":
-          open_url("https://onshape.com", shift);
-          return;
-        default:
-          break;
-      }
-      let got_url = goto_items[cmd]
-      if (got_url != null) {
-        open_url(got_url, shift);
-        return;
-      }
-      let vakken_url = vakken[cmd];
-      if (vakken_url != null) {
-        open_url(vakken_url, shift);
-        return;
-      }
-    }, "quick:");
-  }
-});
 async function apply(){
   let settingsData = JSON.parse(window.localStorage.getItem("settingsdata"))
   console.log(settingsData)
