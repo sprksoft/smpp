f3ef6bf54ae04e646da78e22eb272f89cfff42aa
frickingbird8002 <sakul8002@icloud.com>
2024-01-20 12:30:08 +0100

    more optimization

--- delijndecoder.js
+++ delijndecoder.js
@@ -62,47 +62,54 @@ function getchoice(data) {
 }
 
 function decodehalte() {
-  leftContainer = document.getElementById("leftcontainer")
-  if (leftContainer) {
+  const leftContainer = document.getElementById("leftcontainer");
+  if (!leftContainer) return;
 
-    leftContainer.innerHTML = " "
-    leftContainer.innerHTML = `<div id="top_lijn_app"><h2 class="delijn_app_title">Zoek naar halte:</h2><div class="textandbutton">
-      <input class="popupinput" id="haltetext" type="text"></input><button type="submit" class="searchbutton"id="searchbutton">
-      <svg width="20" height="20" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-      <path d="M16.0073 9.00364C16.0073 12.8716 12.8716 16.0073 9.00364 16.0073C5.13564 16.0073 2 12.8716 2 9.00364C2 
-      5.13564 5.13564 2 9.00364 2C12.8716 2 16.0073 5.13564 16.0073 9.00364Z" class=stroke1 stroke-width="4">
-      </path><rect x="16.594" y="12.8062" width="11.8729" height="3.95764" rx="1.97882" transform="rotate(45 16.594 12.8062)" class=st1>
-      </rect></svg></button></div></div>
-      `
-    searchbutton = document.getElementById('searchbutton')
-    const leftContainerbottom = document.createElement("div")
-    leftContainerbottom.innerHTML = `<div id="leftContainerbottom"><p class=lijninfo>Loading...</p></div>`
-    leftContainer.appendChild(leftContainerbottom)
-    searchbutton.addEventListener("click", function () {
-      fetchHaltesData(document.getElementById("haltetext").value)
+  leftContainer.innerHTML = `
+    <div id="top_lijn_app">
+      <h2 class="delijn_app_title">Zoek naar halte:</h2>
+      <div class="textandbutton">
+        <input class="popupinput" id="haltetext" type="text">
+        <button type="submit" class="searchbutton" id="searchbutton">
+          <svg width="20" height="20" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path d="M16.0073 9.00364C16.0073 12.8716 12.8716 16.0073 9.00364 16.0073C5.13564 16.0073 2 12.8716 2 9.00364C2 
+            5.13564 5.13564 2 9.00364 2C12.8716 2 16.0073 5.13564 16.0073 9.00364Z" class="stroke1" stroke-width="4"></path>
+            <rect x="16.594" y="12.8062" width="11.8729" height="3.95764" rx="1.97882" transform="rotate(45 16.594 12.8062)" class="st1"></rect>
+          </svg>
+        </button>
+      </div>
+    </div>`;
+
+  const searchbutton = document.getElementById('searchbutton');
+  const leftContainerbottom = document.createElement("div");
+  leftContainerbottom.innerHTML = `<div id="leftContainerbottom"><p class=lijninfo>Loading...</p></div>`;
+  leftContainer.appendChild(leftContainerbottom);
+
+  searchbutton.addEventListener("click", function () {
+    const haltetext = document.getElementById("haltetext").value;
+    fetchHaltesData(haltetext);
+    if (document.getElementById("lijncard0")) {
+      decodehalte();
+    }
+  });
+
+  const input = document.getElementById("haltetext");
+  input.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      const haltetext = input.value;
+      fetchHaltesData(haltetext);
       if (document.getElementById("lijncard0")) {
-        decodehalte()
+        decodehalte();
       }
-    })
-    var input = document.getElementById("haltetext");
+    }
+  });
 
-    input.addEventListener("keyup", function (event) {
-      if (event.key === "Enter") {
-        fetchHaltesData(document.getElementById("haltetext").value)
-        if (document.getElementById("lijncard0")) {
-          decodehalte()
-        }
-      }
-    });
-    let lijnData = JSON.parse(window.localStorage.getItem("lijnData"));
-    if (lijnData != undefined) {
-      fetchData(lijnData.entiteitnummer, lijnData.haltenummer)
-    }
-    else {
-      leftContainerbottom.innerHTML = `<div id="leftContainerbottom"><p class=lijninfo>Zoek naar een halte aub.</p></div>`
-    }
+  const lijnData = JSON.parse(localStorage.getItem("lijnData"));
+  if (lijnData) {
+    fetchData(lijnData.entiteitnummer, lijnData.haltenummer);
+  } else {
+    leftContainerbottom.innerHTML = `<div id="leftContainerbottom"><p class=lijninfo>Zoek naar een halte aub.</p></div>`;
   }
 }
-
 
 
