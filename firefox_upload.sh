ab0dec9fdb7b8d190a509c3535748a2426c0fc80
ldev <ldev@ldev.eu.org>
2024-02-11 17:35:14 +0100

    forgot to change debug domain in firefox_upload (added debug mode also)

--- firefox_upload.sh
+++ firefox_upload.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Upload de firefox versie naar ldev.eu.org
-# Usage: firefox_upload.sh <session_token>
+# Usage: firefox_upload.sh <session_token> [-d|--debug]
 
 ses_token=$1
 if [[ -z "$ses_token" ]] ; then
   echo "Session token required"
   exit 1
+fi
+
+domain="https://ldev.eu.org"
+if [ "$2" = "-d" ] | [ "$2" = "--debug" ] ; then
+  domain="http://localhost:8080"
 fi
 
 echo "Deleting old artifacts..."
@@ -17,4 +22,4 @@ ver=$(ls web-ext-artifacts | rg "smartschool_-([0-9]*.[0-9]*.[0-9]*).zip" -r '$1
 
 echo "version: $ver"
 echo "Uploading..."
-curl --data-binary web-ext-artifacts/smartschool_-$ver.zip -H "Cookie:session=$ses_token" "http://localhost:8080/firefox/smpp?v=$ver"
+curl --data-binary web-ext-artifacts/smartschool_-$ver.zip -H "Cookie:session=$ses_token" "$domain/firefox/smpp?v=$ver"
