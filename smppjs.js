8e0080d5d46c07dafbcf1dd6213d6aa3518a9aa2
frickingbird8002 <sakul8002@icloud.com>
2024-03-18 22:15:51 +0100

    yippieee

--- smppjs.js
+++ smppjs.js
@@ -79,6 +79,33 @@ async function apply() {
   if (show_scores == undefined) {
     show_scores = false;
   }
+document.querySelector('.topnav').insertBefore(document.querySelector('[data-links]'), document.querySelector('[data-courses]'));
+
+// Create the SVG markup
+const svgMarkup = `
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class=st1 height="18px" width="18px" version="1.1" id="Layer_1" viewBox="0 0 459.334 459.334" xml:space="preserve">
+<g>
+	<g>
+		<g>
+			<path d="M175.216,404.514c-0.001,0.12-0.009,0.239-0.009,0.359c0,30.078,24.383,54.461,54.461,54.461     s54.461-24.383,54.461-54.461c0-0.12-0.008-0.239-0.009-0.359H175.216z"/>
+			<path d="M403.549,336.438l-49.015-72.002c0-22.041,0-75.898,0-89.83c0-60.581-43.144-111.079-100.381-122.459V24.485     C254.152,10.963,243.19,0,229.667,0s-24.485,10.963-24.485,24.485v27.663c-57.237,11.381-100.381,61.879-100.381,122.459     c0,23.716,0,76.084,0,89.83l-49.015,72.002c-5.163,7.584-5.709,17.401-1.419,25.511c4.29,8.11,12.712,13.182,21.887,13.182     H383.08c9.175,0,17.597-5.073,21.887-13.182C409.258,353.839,408.711,344.022,403.549,336.438z"/>
+		</g>
+	</g>
+</g>
+</svg>
+`;
+
+// Get reference to the button element
+const notifsButton = document.querySelector('.js-btn-notifs');
+
+// Get reference to the existing span element
+const textSpan = notifsButton.querySelector('span');
+
+// Replace the button content with the SVG and the existing span
+notifsButton.innerHTML = svgMarkup
+notifsButton.appendChild(textSpan);
+
+
 
 window.addEventListener('load', async function (){
   if (show_scores) {
