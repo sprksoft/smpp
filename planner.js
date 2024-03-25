020cca067b0159a5e6094d7232efe600ab76161c
frickingbird8002 <sakul8002@icloud.com>
2024-03-25 09:41:01 +0100

    planner should work for everybody always

--- planner.js
+++ planner.js
@@ -1,9 +1,10 @@
 const pixelsPerMinute = 1.46;
-async function fetchPlannerData(){
+async function fetchPlannerData(date, user){
     try {
         var currentUrl = window.location.href;
         school_name = currentUrl.split("/")[2]
-        const url = `https://${school_name}/planner/api/v1/planned-elements/user/1045_6232_0?from=2024-03-25&to=2024-03-25`;
+        const url = `https://${school_name}/planner/api/v1/planned-elements/user/${user}?from=${date}&to=${date}`;
+        console.log(`https://${school_name}/planner/api/v1/planned-elements/user/${user}?from=${date}&to=${date}`)
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error(`Failed to fetch planner data`);
@@ -20,11 +21,42 @@ const calculateElementHeight = (startTime, endTime) => {
     const durationInMinutes = durationInSeconds / 60;
     return durationInMinutes * pixelsPerMinute;
 };
+async function getDateInCorrectFormat() {
+    let currentDate = new Date();
+    
+    // Check if it's after 6pm on Friday or on Saturday/Sunday
+    if (currentDate.getDay() === 5 && currentDate.getHours() >= 18) {
+        currentDate.setDate(currentDate.getDate() + 3); // Set date to upcoming Monday
+    } else if (currentDate.getDay() === 6 || currentDate.getDay() === 0) {
+        currentDate.setDate(currentDate.getDate() + (8 - currentDate.getDay())); // Set date to upcoming Monday
+    }
+
+    // Format the date as "DD/MM/YYYY"
+    let day = currentDate.getDate().toString().padStart(2, '0');
+    let month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    let year = currentDate.getFullYear();
+
+    return `${day}-${month}-${year}`;
+}
 
 async function ShowPlanner() {
     const container = document.getElementById('leftcontainer');
     container.innerHTML = ''; 
-    var data = await fetchPlannerData();
+    if (document.getElementById('datePickerMenu')) {
+        // Get the value of the "plannerurl" attribute
+        var plannerUrl = document.getElementById('datePickerMenu').getAttribute('plannerurl');
+    
+        // Check if plannerUrl is not null
+        if (plannerUrl) {
+            // Output the plannerUrl
+            console.log("Planner URL:", plannerUrl);
+        } else {
+            console.error("Attribute 'plannerurl' not found in the element.");
+        }
+    } else {
+        console.error("Element with id 'datePickerMenu' not found.");
+    }
+    var data = await fetchPlannerData(await getDateInCorrectFormat(),"1045_6232_0");
     console.log(data)
     if (!data || data.length === 0) {
         document.getElementById('leftcontainer').innerHTML = 'No planner data available.';
