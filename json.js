89a7d9858033ab73daede1766e7d5375371c1d0a
BBjarne <bond.bjarne@gmail.com>
2024-05-29 11:49:19 +0200

    aaaaa

--- json.js
+++ json.js
@@ -175,27 +175,27 @@ const messageSvg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://cre
 const colorpickersHTML = `
 <div class="color-picker-container">
 <input type="color" class="color-pickersmpp" id="colorPicker1">
-<span class="color-label">Base0</span>
+<span class="color-label">base0</span>
 </div>
 <div class="color-picker-container">
 <input type="color" class="color-pickersmpp" id="colorPicker2">
-<span class="color-label">Base1</span>
+<span class="color-label">base1</span>
 </div>
 <div class="color-picker-container">
 <input type="color" class="color-pickersmpp" id="colorPicker3">
-<span class="color-label">Base2</span>
+<span class="color-label">base2</span>
 </div>
 <div class="color-picker-container">
 <input type="color" class="color-pickersmpp" id="colorPicker4">
-<span class="color-label">Base3</span>
+<span class="color-label">base3</span>
 </div>
 <div class="color-picker-container">
 <input type="color" class="color-pickersmpp" id="colorPicker5">
-<span class="color-label">Accent</span>
+<span class="color-label">accent</span>
 </div>
 <div class="color-picker-container">
 <input type="color" class="color-pickersmpp" id="colorPicker6">
-<span class="color-label">Text</span>
+<span class="color-label">text</span>
 </div>`
 const popupsettingHTML = `<head>
 <meta charset="utf-8">
@@ -263,7 +263,10 @@ const popupsettingHTML = `<head>
   <button class="popupinput" class="backgroundfile" id="backgroundfilebutton"><svg width="30px" height="30px" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg">
   <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9 13H15M12.0627 6.06274L11.9373 5.93726C11.5914 5.59135 11.4184 5.4184 11.2166 5.29472C11.0376 5.18506 10.8425 5.10425 10.6385 5.05526C10.4083 5 10.1637 5 9.67452 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V10.2C21 9.0799 21 8.51984 20.782 8.09202C20.5903 7.71569 20.2843 7.40973 19.908 7.21799C19.4802 7 18.9201 7 17.8 7H14.3255C13.8363 7 13.5917 7 13.3615 6.94474C13.1575 6.89575 12.9624 6.81494 12.7834 6.70528C12.5816 6.5816 12.4086 6.40865 12.0627 6.06274Z" class="st4" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"></path> </g>
   </svg></button>
-  <input type="range" min="0" max="20" value="0" class="sliderblur" id="mySlider">
+  <div class="color-picker-container">
+    <input type="range" min="0" max="20" value="0" class="sliderblur" id="mySlider">
+    <span class="color-label" id="blurPlaats">blur</span>
+  </div>
 </div>
 <div class="textandbutton" id="errormessagesmpp"></div>
 <h3 class="popuptitles">Weather Overlay:</h3>
