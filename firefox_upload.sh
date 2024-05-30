36100f2e9a83252ed70cbc4f312c37efbfde31d4
ldev <ldev@ldev.eu.org>
2024-05-30 16:27:07 +0200

    firefox_upload script improve

--- firefox_upload.sh
+++ firefox_upload.sh
@@ -2,16 +2,26 @@
 # Upload de firefox versie naar ldev.eu.org
 # Usage: firefox_upload.sh <session_token> [-d|--debug]
 
-ses_token=$1
-if [[ -z "$ses_token" ]] ; then
-  echo "Session token required"
-  exit 1
-fi
+TOKEN_GET_URL="https://ldev.eu.org/sestoken"
 
 domain="https://ldev.eu.org"
 if [ "$2" = "-d" ] | [ "$2" = "--debug" ] ; then
   domain="http://localhost:8080"
 fi
+
+
+ses_token=$1
+if [[ -z "$ses_token" ]] ; then
+  echo "Session token required"
+  echo "Go to $TOKEN_GET_URL and copy the token here"
+  xdg-open $TOKEN_GET_URL
+  echo -n "token: "
+  read ses_token
+  if [[ -z "$ses_token" ]] ; then
+    ses_token="$(wl-paste)"
+  fi
+fi
+
 
 echo "Deleting old artifacts..."
 rm -rf web-ext-artifacts/*
