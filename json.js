60d04227ace07eebff4fc74546e1a971650f0441
frickingbird8002 <sakul8002@icloud.com>
2024-07-31 01:04:09 +0200

    added some splashtexts

--- json.js
+++ json.js
@@ -1,6 +1,7 @@
 const themes = {
   "default": {
     "--color-accent": "#c9d2e6",
+    "--color-splashtext": "#c9d2e6",
     "--color-text": "#C2BAB2",
     "--color-base00": "#191817",
     "--color-base01": "#232020",
@@ -25,7 +26,8 @@ const themes = {
     "--color-base03": "#7b93b4",
     "--color-homepage-sidebars-bg": "#02020540",
     "--loginpage-image": "url(https://wallpaperaccess.com/full/1474688.jpg)",
-    "--darken-background": "rgba(0,0,0,0.1)"
+    "--darken-background": "rgba(0,0,0,0.1)",
+    "--color-splashtext": "#080a12"
   },
   "ldev": {
     "--color-accent": "#ffd5a0",
@@ -33,7 +35,8 @@ const themes = {
     "--color-hover-border": "var(--color-base03)",
     "--color-homepage-sidebars-bg": "var(--color-base00)",
     "--loginpage-image": "url(https://i.redd.it/yfssdsfosao11.png)",
-    "--darken-background": "rgba(0,0,0,0.25)"
+    "--darken-background": "rgba(0,0,0,0.25)",
+    "--color-splashtext": "#ffd5a0"
   },
   "purple": {
     "--color-accent": "#bd4fb3",
@@ -42,7 +45,8 @@ const themes = {
     "--color-base01": "#130332",
     "--color-base02": "#250654",
     "--color-base03": "#3f0a74",
-    "--loginpage-image": "url(https://www.hdwallpapers.in/download/macos_monterey_shapes_hd_macos-2560x1440.jpg)"
+    "--loginpage-image": "url(https://www.hdwallpapers.in/download/macos_monterey_shapes_hd_macos-2560x1440.jpg)",
+    "--color-splashtext": "#dfdeff"
   },
   "stalker": {
     "--color-accent": "#d6574e",
@@ -51,7 +55,8 @@ const themes = {
     "--color-base01": "#371b19",
     "--color-base02": "#5c1c1a",
     "--color-base03": "#823530",
-    "--loginpage-image": "url(https://media.timeout.com/images/102945740/image.jpg)"
+    "--loginpage-image": "url(https://media.timeout.com/images/102945740/image.jpg)",
+    "--color-splashtext": "#efc2c3"
   },
   "chocolate": {
     "--color-accent": "#cdbcb4",
@@ -61,7 +66,8 @@ const themes = {
     "--color-base02": "#3b2e2e",
     "--color-base03": "#4c3e3e",
     "--loginpage-image": "url(https://www.hdwallpapers.in/download/wet_brown_leaves_hd_dark_aesthetic-HD.jpg)",
-    "--darken-background": "rgba(0,0,0,0.3)"
+    "--darken-background": "rgba(0,0,0,0.3)",
+    "--color-splashtext": "#d3b199"
   },
   "fall": {
     "--color-accent": "#f8c791",
@@ -71,7 +77,8 @@ const themes = {
     "--color-base02": "#7b3f31",
     "--color-base03": "#bf6f51",
     "--loginpage-image": "url(https://wallpapercave.com/wp/wp7464660.jpg)",
-    "--darken-background": "rgba(0,0,0,0.3)"
+    "--darken-background": "rgba(0,0,0,0.3)",
+    "--color-splashtext": "#ffe5d3"
   },
   "winter": {
     "--color-accent": "#8aadb6",
@@ -80,7 +87,8 @@ const themes = {
     "--color-base01": "#152f47",
     "--color-base02": "#345f7f",
     "--color-base03": "#5687b6",
-    "--loginpage-image": "url(https://th.bing.com/th/id/R.fd4990dbff7b2d998a61b5a60b6b1949?rik=TkC2r3hdP1Ma9g&pid=ImgRaw&r=0)"
+    "--loginpage-image": "url(https://th.bing.com/th/id/R.fd4990dbff7b2d998a61b5a60b6b1949?rik=TkC2r3hdP1Ma9g&pid=ImgRaw&r=0)",
+    "--color-splashtext": "#7bcfe2"
   },
   "birb": {
     "--color-accent": "#8590aacc",
@@ -89,7 +97,8 @@ const themes = {
     "--color-base01": "#18191d",
     "--color-base02": "#232428",
     "--color-base03": "#3a3c44",
-    "--loginpage-image": "url(https://wallpapercave.com/wp/wp4673203.jpg)"
+    "--loginpage-image": "url(https://wallpapercave.com/wp/wp4673203.jpg)",
+    "--color-splashtext": "#7089a4"
   },
   "matcha": {
     "--color-accent": "#d1f8e7",
@@ -99,7 +108,8 @@ const themes = {
     "--color-base02": "#456046",
     "--color-base03": "#4f7a51",
     "--loginpage-image": "url(https://wallpapercave.com/wp/wp9313069.jpg)",
-    "--darken-background": "rgba(0,0,0,0.15)"
+    "--darken-background": "rgba(0,0,0,0.15)",
+    "--color-splashtext": "#8ab4a7"
   },
   "mountain": {
     "--color-accent": "#f8f8fa",
@@ -109,7 +119,8 @@ const themes = {
     "--color-base02": "#52647c",
     "--color-base03": "#8294ac",
     "--loginpage-image": "url(https://hdqwalls.com/download/everest-3840x2160.jpg)",
-    "--darken-background": "rgba(0,0,0,0.15)"
+    "--darken-background": "rgba(0,0,0,0.15)",
+    "--color-splashtext": "#030c18"
   },
   "vax": {
     "--color-accent": "#7a1600",
@@ -119,7 +130,8 @@ const themes = {
     "--color-base02": "#b9815d",
     "--color-base03": "#caa288",
     "--loginpage-image": "url(https://wallpapers.com/images/hd/star-wars-place-ztno3exzqff0m0ci.webp)",
-    "--darken-background": "rgba(0,0,0,0.15)"
+    "--darken-background": "rgba(0,0,0,0.15)",
+    "--color-splashtext": "#7a1600"
   },
   "sand": {
     "--color-accent": "#8097c5",
@@ -129,7 +141,8 @@ const themes = {
     "--color-base02": "#544848",
     "--color-base03": "#634f4e",
     "--loginpage-image": "url(https://hdqwalls.com/wallpapers/macos-mojave-dusk-mode-stock-el.jpg)",
-    "--darken-background": "rgba(0,0,0,0.15)"
+    "--darken-background": "rgba(0,0,0,0.15)",
+    "--color-splashtext": "#e0c7b5"
   },
   "galaxy": {
     "--color-accent": "#daa0ef",
@@ -139,7 +152,8 @@ const themes = {
     "--color-base02": "#474e5f",
     "--color-base03": "#5c667d",
     "--loginpage-image": "url(https://i.redd.it/u80014ygsea51.png)",
-    "--darken-background": "rgba(0,0,0,0.3)"
+    "--darken-background": "rgba(0,0,0,0.3)",
+    "--color-splashtext": "#e1bbef"
   }
 }
 const discordSvg = `<div class="bigdiscordbutton" style="position: fixed; bottom: 0; left: 0; width: 40px; height: 40px; border: 2px solid var(--color-base00); border-radius:10px; background-color: var(--color-base01);">
