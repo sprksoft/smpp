e19f98f104d7d2a03db1783b9d0bd0eccd8eb086
ldev <ldev@ldev.eu.org>
2025-01-25 17:54:22 +0100

    small changes to firefox_upload

--- firefox_upload.sh
+++ firefox_upload.sh
@@ -2,15 +2,16 @@
 # Upload de firefox versie naar ldev.eu.org
 # Usage: firefox_upload.sh <session_token> [-d|--debug]
 
-TOKEN_GET_URL="https://ldev.eu.org/sestoken"
-
-domain="https://ldev.eu.org"
-if [ "$2" = "-d" ] | [ "$2" = "--debug" ] ; then
-  domain="http://localhost:8080"
+root="https://ldev.eu.org"
+ses_token=$1
+if [ "$1" = "-d" ] | [ "$1" = "--debug" ] ; then
+  echo "debug mode"
+  root="http://localhost:80"
+  ses_token=$2
 fi
 
+TOKEN_GET_URL="$root/sestoken"
 
-ses_token=$1
 if [[ -z "$ses_token" ]] ; then
   echo "Session token required"
   echo "Go to $TOKEN_GET_URL and copy the token here"
@@ -32,4 +33,4 @@ ver=$(ls web-ext-artifacts | rg "smartschool_-([0-9]*.[0-9]*.[0-9]*).zip" -r '$1
 
 echo "version: $ver"
 echo "Uploading..."
-curl --data-binary @web-ext-artifacts/smartschool_-$ver.zip -H "Cookie:session=$ses_token" "$domain/firefox/smpp?v=$ver"
+curl --data-binary @web-ext-artifacts/smartschool_-$ver.zip -H "Cookie:session=$ses_token" "$root/firefox/smpp?v=$ver"
