ef68dd7f65255469e8bc6d97cd4c9ec4fdecacb2
ldev <ldev@ldev.eu.org>
2025-04-04 22:14:55 +0200

    er is incest in onze git tree

--- login.js
+++ login.js
@@ -1,5 +1,14 @@
 /* vim:set shiftwidth=4: */
 
+function updateLoginPanel() {
+  let login_app_left = document.querySelector('.login-app__left');
+  login_app_left.innerHTML = ' ';
+  document.getElementsByClassName('login-app__platform-indicator')[0].innerHTML = '<h1 class="logintitle">Smartschool ++</h1>';
+  document.getElementsByClassName('login-app__title--separator')[0].innerHTML = `<button type="button" class="white_text_button" id="showmore">More</button>`;
+  document.getElementById('showmore').addEventListener('click', showmore)
+  function showmore() { style.setProperty('--show-options', 'flex'); document.getElementById("showmore").style.display = "none" }
+}
+
 function add_splash_text() {
     var login_app = document.querySelector('.login-app')
     var splash_text_element = document.createElement('div')
