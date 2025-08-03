721b56d071c1ae1449f846ae8dc9b4efb92f9975
ldev <ldev@ldev.eu.org>
2025-08-03 16:54:22 +0200

    fixes to ff_up.sh

--- build/ff_up.sh
+++ build/ff_up.sh
@@ -3,6 +3,8 @@ set -e
 # Upload de firefox versie naar ldev.eu.org
 # Usage: ff_up.sh <key>
 
+cd $(dirname $0)
+
 root="https://ldev.eu.org"
 key=$1
 if [[ "$1" == "-d" ]] || [[ "$1" == "--debug" ]] ; then
@@ -12,8 +14,8 @@ if [[ "$1" == "-d" ]] || [[ "$1" == "--debug" ]] ; then
 fi
 
 echo "Validating manifest"
-pkg_id=$(cat manifest.json | jq -r .browser_specific_settings.gecko.id)
-if [[ cat manifest.json | jq -r ".browser_specific_settings.gecko.update_url == \"https://ldev.eu.org/firefox/updates.json\"" == "false" ]] ; then
+pkg_id=$(cat ../manifest.json | jq -r .browser_specific_settings.gecko.id)
+if [[ $(cat ../manifest.json | jq -r ".browser_specific_settings.gecko.update_url == \"https://ldev.eu.org/firefox/updates.json\"") == "false" ]] ; then
   printf 'validation failed\n' 1>&2
   exit 1
 fi
@@ -30,4 +32,4 @@ ver=$(ls web-ext-artifacts | rg "smartschool_-([0-9]*.[0-9]*.[0-9]*).zip" -r '$1
 echo "=== Done ==="
 echo "version: $ver"
 echo "Uploading to $root..."
-curl --data-binary @web-ext-artifacts/smartschool_-$ver.zip -H "Cookie:session=$ses_token" "$root/firefox/$pkg_id?v=$ver"
+curl --data-binary @web-ext-artifacts/smartschool_-$ver.zip -H "Authorization:Bearer $key" "$root/firefox/$pkg_id?v=$ver"
