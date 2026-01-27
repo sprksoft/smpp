4299e3f6942a39f2b33f0e939a2db447c0662b61
ldev <ldev@ldev.eu.org>
2026-01-27 15:46:56 +0100

    linux version of the optimize script

--- extension/media/theme-backgrounds/compressJPG's.sh
+++ extension/media/theme-backgrounds/compressJPG's.sh
@@ -3,10 +3,10 @@ OUTDIR="optimized"
 mkdir -p $OUTDIR
 
 # === Loop through all JPG files ===
-for file in *.{jpg,jpeg} ; do
+for file in *.jpg ; do
   echo Procesing $file
   # === Convert and compress ===
-  magick "$file" -resize "2560x1440>" -strip -interlace Plane -quality 85 "$outdir/$file.jpg"
+  magick "$file" -resize "2560x1440>" -strip -interlace Plane -quality 85 "$OUTDIR/$file.jpg"
 done
 
 echo "Done! Optimized images saved in $OUTDIR"
